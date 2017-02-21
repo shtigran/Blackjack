02d75165f9c03310efa2d9ea24ecf25343eeb5bd
shtigran <shakhbekyantigran@gmail.com>
2017-02-21 21:35:22 +0400

    Start

---
+++ Deck Of Cards/Deck Of Cards/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deck_Of_Cards
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}

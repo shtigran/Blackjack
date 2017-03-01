4b200defd0f49c67295f7fffb5686cda536dfd29
shtigran <shakhbekyantigran@gmail.com>
2017-03-01 10:16:27 +0400

    Changed HandingOne method for 52 cardds

--- Deck Of Cards/Deck Of Cards/Program.cs
+++ Deck Of Cards/Deck Of Cards/Program.cs
@@ -127,7 +127,8 @@ namespace Deck_Of_Cards
                 }
                 sum1 += b;
               }
-              else {
+              else
+              {
 
                 str2 += $" *{cards[j]} of {suit[i]}*";
                 int b = 0;
@@ -181,84 +182,114 @@ namespace Deck_Of_Cards
     }
 
     public void HandingOne(ref int n, ref int sum1, ref int sum2, ref string str1, ref string str2)
+    {
+      int k = n + 1;
+
+      while (n <= k)
+      {
+        for (int i = 0; i < 4; i++)
         {
-           int k = n + 1;
-
-            while (n <= k)
+          for (int j = 0; j < 13; j++)
+          {
+            if (arr[i, j] == n)
             {
-                for (int i = 0; i < 4; i++)
+              if (n % 2 != 0)
+              {
+                str1 += $" *{cards[j]} of {suit[i]}*";
+                int b = 0;
+                switch (cards[j])
                 {
-                    for (int j = 0; j < 13; j++)
-                    {
-                        if (arr[i, j] == n)
-                        {
-                            if (n % 2 != 0)
-                            {
-                                str1 += $" *{cards[j]} of {suit[i]}*";
-                                int b = 0;
-                                switch (cards[j])
-                                {
-                                    case "Seven":
-                                        b = 7;
-                                        break;
-                                    case "Eight":
-                                        b = 8;
-                                        break;
-                                    case "Nine":
-                                        b = 9;
-                                        break;
-                                    case "Ten":
-                                    case "Jack":
-                                    case "Queen":
-                                    case "King":
-                                        b = 10;
-                                        break;
-                                    case "Ace":
-                                        b = 11;
-                                        break;
-                                }
-                                sum1 += b;
-                            }
-                            else
-                            {
-
-                                str2 += $" *{cards[j]} of {suit[i]}*";
-                                int b = 0;
-                                switch (cards[j])
-                                {
-                                    case "Seven":
-                                        b = 7;
-                                        break;
-                                    case "Eight":
-                                        b = 8;
-                                        break;
-                                    case "Nine":
-                                        b = 9;
-                                        break;
-                                    case "Ten":
-                                    case "Jack":
-                                    case "Queen":
-                                    case "King":
-                                        b = 10;
-                                        break;
-                                    case "Ace":
-                                        b = 11;
-                                        break;
-                                }
-                                sum2 += b;
-
-                            }
-                        }
-                    }
+                  case "Two":
+                    b = 2;
+                    break;
+                  case "Three":
+                    b = 3;
+                    break;
+                  case "Four":
+                    b = 4;
+                    break;
+                  case "Five":
+                    b = 5;
+                    break;
+                  case "Six":
+                    b = 6;
+                    break;
+                  case "Seven":
+                    b = 7;
+                    break;
+                  case "Eight":
+                    b = 8;
+                    break;
+                  case "Nine":
+                    b = 9;
+                    break;
+                  case "Ten":
+                  case "Jack":
+                  case "Queen":
+                  case "King":
+                    b = 10;
+                    break;
+                  case "Ace":
+                    b = 11;
+                    break;
                 }
-                n++;
-            }
-            Console.WriteLine($"\nThe Player has: {str1}\n(sum is: {sum1})\n\n");
-            Console.WriteLine($"The Computer has: {str2}\n(sum is: {sum2})\n");
+                sum1 += b;
+              }
+              else
+              {
 
+                str2 += $" *{cards[j]} of {suit[i]}*";
+                int b = 0;
+                switch (cards[j])
+                {
+                  case "Two":
+                    b = 2;
+                    break;
+                  case "Three":
+                    b = 3;
+                    break;
+                  case "Four":
+                    b = 4;
+                    break;
+                  case "Five":
+                    b = 5;
+                    break;
+                  case "Six":
+                    b = 6;
+                    break;
+                  case "Seven":
+                    b = 7;
+                    break;
+                  case "Eight":
+                    b = 8;
+                    break;
+                  case "Nine":
+                    b = 9;
+                    break;
+                  case "Ten":
+                  case "Jack":
+                  case "Queen":
+                  case "King":
+                    b = 10;
+                    break;
+                  case "Ace":
+                    b = 11;
+                    break;
+                }
+                sum2 += b;
+
+              }
+            }
+          }
         }
+        n++;
+      }
+      Console.WriteLine($"\nThe Player has: {str1}\n(sum is: {sum1})\n\n");
+      Console.WriteLine($"The Computer has: {str2}\n(sum is: {sum2})\n");
 
     }
+
+  }
 
 
   class Game
@@ -277,7 +308,7 @@ namespace Deck_Of_Cards
 
     }
 
-    public bool check (int sumPlayer, int sumComp)
+    public bool check(int sumPlayer, int sumComp)
     {
       if (sumPlayer == sumComp && sumComp == 21)
       {
@@ -316,7 +347,7 @@ namespace Deck_Of_Cards
 
       while (flag)
       {
-        Console.WriteLine( "*****Welcome to Blackjack*****" );
+        Console.WriteLine("*****Welcome to Blackjack*****");
         Console.WriteLine("\nWe Start Our Game!!!\n");
         Console.WriteLine(new string('-', 80));
         Console.WriteLine();
@@ -325,45 +356,45 @@ namespace Deck_Of_Cards
         flag = check(sumPlayer, sumComp); // Checking if any player wins
         if (flag == false)
           break;
-                
-                do
-                {
-                    Console.WriteLine("Do You want any card or check ? (y/n)");
-                    key = Convert.ToChar(Console.ReadLine());
-                    if (key == 'y')
-                    {
 
-                        deck.HandingOne(ref n, ref sumPlayer, ref sumComp, ref cardsPlayer, ref cardsComp);
-                        check(sumPlayer, sumComp);
-                        if (sumPlayer > 21 && sumComp < 21)
-                            Console.WriteLine("You lose!!!");
-                        if (sumComp > 21 && sumPlayer < 21)
-                            Console.WriteLine("You win!!!");
-                        if (sumComp > 21 && sumPlayer > 21)
-                            Console.WriteLine("Draw, try again");
-                        
+        do
+        {
+          Console.WriteLine("Do You want any card or check ? (y/n)");
+          key = Convert.ToChar(Console.ReadLine());
+          if (key == 'y')
+          {
 
-                    }
-                    else
-                    {
-                        flag = false;
-                        if(sumPlayer > sumComp)
-                           Console.WriteLine("You Win!!!");
-                        else if (sumComp > sumPlayer)
-                            Console.WriteLine("You lose!!!");
-                        else Console.WriteLine("Draw, try again");
-                        Console.WriteLine("Good Luck!!!");
-                        break;
-                    }
-
-                }
-                while (sumPlayer < 21 && sumComp < 21);
-
-                flag = false;
+            deck.HandingOne(ref n, ref sumPlayer, ref sumComp, ref cardsPlayer, ref cardsComp);
+            check(sumPlayer, sumComp);
+            if (sumPlayer > 21 && sumComp < 21)
+              Console.WriteLine("You lose!!!");
+            if (sumComp > 21 && sumPlayer < 21)
+              Console.WriteLine("You win!!!");
+            if (sumComp > 21 && sumPlayer > 21)
+              Console.WriteLine("Draw, try again");
 
 
+          }
+          else
+          {
+            flag = false;
+            if (sumPlayer > sumComp)
+              Console.WriteLine("You Win!!!");
+            else if (sumComp > sumPlayer)
+              Console.WriteLine("You lose!!!");
+            else Console.WriteLine("Draw, try again");
+            Console.WriteLine("Good Luck!!!");
+            break;
+          }
 
-            }
+        }
+        while (sumPlayer < 21 && sumComp < 21);
+
+        flag = false;
+
+
+
+      }
 
 
     }
